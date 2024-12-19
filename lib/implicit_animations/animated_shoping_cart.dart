bf21de1f8a803e8629725d85c4a2550eac415f08
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-12-19 22:23:10 +0600

    animation

--- lib/implicit_animations/animated_shoping_cart.dart
+++ lib/implicit_animations/animated_shoping_cart.dart
@@ -8,24 +8,42 @@ class AnimatedShopingCart extends StatefulWidget {
 }
 
 class _AnimatedShopingCartState extends State<AnimatedShopingCart> {
-  // bool Expanded isExpanded
+  bool isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shopping Cart'),
       ),
-      body: Center(
-        child: Container(
-          width: 80.0,
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            isExpanded = !isExpanded;
+          });
+        },
+        child: Center(
+          child: AnimatedContainer(
+            width: isExpanded? 200 : 80.0,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: isExpanded ? Colors.green :  Colors.blue,
+              borderRadius: BorderRadius.circular(isExpanded? 30 : 10.0),
+            ),
+            duration: Duration(milliseconds: 500),
+            child: isExpanded ? Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  Icons.done,
+                  color: Colors.white,
+                ),
+                Text('Added to card', style: TextStyle(color: Colors.white),)
+              ],
+            ) :  Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
           ),
         ),
       ),
